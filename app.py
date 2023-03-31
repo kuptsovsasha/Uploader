c50ee345a0978bac800ee86cdd9624b616362a68
kuptsovsasha <kuptsovsasha@gmail.com>
2023-03-31 15:02:25 +0300

    add download view

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 import time
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
-app_root = os.path.dirname(os.path.abspath(__file__))
 
 
 @app.route('/')
@@ -24,7 +23,7 @@ def upload():
     end_time = time.time()
     time_spent = end_time - start_time
 
-    download_link = f'http://206.189.83.222/{os.path.join(app_root, folder_name, filename)}'
+    download_link = f'http://206.189.83.222:5000/download_file/{current_time}/{filename}'
     return f'''
     <!doctype html>
     <h1>The file {filename} was uploaded successfully.</h1>
@@ -32,6 +31,17 @@ def upload():
     <h2>You can download the file with link below:</h2>
     <a href="{download_link}">{download_link}</a>
     '''
+
+
+@app.route('/download_file/<path:file_path>')
+def download_file(file_path):
+    print(file_path)
+    uploads_dir = os.path.join(os.getcwd(), 'uploads')
+    file_path = os.path.join(uploads_dir, file_path)
+    if os.path.isfile(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        return f"The requested file '{file_path}' does not exist."
 
 
 if __name__ == '__main__':
