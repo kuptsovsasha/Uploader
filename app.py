c5d449927f2ebce1ac4738e1dd79d70c91a3bf0a
kuptsovsasha <kuptsovsasha@gmail.com>
2023-03-31 10:22:08 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import time
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    start_time = time.time()
+    file = request.files['file']
+    current_time = time.strftime('%Y-%m-%d_%H:%M:%S')
+    folder_name = os.path.join('uploads', current_time)
+    os.makedirs(folder_name) # create a new folder based on the current time
+    filename = file.filename
+
+    file.save(os.path.join(folder_name, filename))
+    end_time = time.time()
+    time_spent = end_time - start_time
+
+    download_link = f'http://206.189.83.222/{folder_name}/{filename}'
+    return f'''
+    <!doctype html>
+    <h1>The file {filename} was uploaded successfully.</h1>
+    <h2>It took {time_spent} seconds to upload the file.</h2>
+    <h2>You can download the file with link below:</h2>
+    <a href="{download_link}">{download_link}</a>
+    '''
+
+
+if __name__ == '__main__':
+    app.run()
